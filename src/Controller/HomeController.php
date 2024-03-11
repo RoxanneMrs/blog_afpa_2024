581501e5d25218898c96fa8d252af1274a4987ee
Roxanne Marais <roxanne.mar@hotmail.com>
2024-03-11 16:57:13 +0100

    Gestion des pages articles et de la connexion/déconnexion d'un user/admin

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Repository\ArticleRepository;
+use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,14 +15,12 @@ class HomeController extends AbstractController
     // symfony dit : si tu as /home tu appelles cette fonction, et tu affiches ceci
     // controller_name = homeController (variables de vues)
     #[Route('/', name: 'app_home')]
-    public function index(EntityManagerInterface $entityManager): Response
-    {
-
-        $categories = $entityManager->getRepository(Category::class)->findAll();
-
+    public function index(ArticleRepository $articleRepository, CategoryRepository $categoryRepository): Response {
+        
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'categories' => $categories,
+            'categories' => $categoryRepository->findAll(),
+            'articles' => $articleRepository->findAll(),
             'fabrice' => 'Hey voici ma variable de vue Fabrice',
         ]);
     }
