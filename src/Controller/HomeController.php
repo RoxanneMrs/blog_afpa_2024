4d446d0bc735124898be66cc08dc706d04ca13f7
Roxanne Marais <roxanne.mar@hotmail.com>
2024-03-12 13:42:13 +0100

    gestion mdp et pagination

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,7 +6,9 @@ use App\Entity\Category;
 use App\Repository\ArticleRepository;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,13 +17,23 @@ class HomeController extends AbstractController
     // symfony dit : si tu as /home tu appelles cette fonction, et tu affiches ceci
     // controller_name = homeController (variables de vues)
     #[Route('/', name: 'app_home')]
-    public function index(ArticleRepository $articleRepository, CategoryRepository $categoryRepository): Response {
+    public function index(ArticleRepository $articleRepository, 
+    CategoryRepository $categoryRepository,
+    PaginatorInterface $paginator,
+    Request $request ): Response {
+
+        
+    $articles = $paginator->paginate(
+        $articleRepository->findAll(), /* query NOT result */
+        $request->query->getInt('page', 1), /*page number*/
+        2 /*limit per page*/
+    );
         
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'categories' => $categoryRepository->findAll(),
-            'articles' => $articleRepository->findAll(),
-            'fabrice' => 'Hey voici ma variable de vue Fabrice',
+            'articles' => $articles,
+            // 'fabrice' => 'Hey voici ma variable de vue Fabrice',
         ]);
     }
 }
