ed7a0b8b68529e15479b09eaebc6a58dc003b7d3
Roxanne Marais <roxanne.mar@hotmail.com>
2024-03-13 12:07:30 +0100

    Gestion des erreurs

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -36,6 +36,21 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'boolean')]
     private $isVerified = false;
 
+    #[ORM\Column(length: 255)]
+    private ?string $address = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $phone_number = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $first_name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $code_postal = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -119,6 +134,66 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setIsVerified(bool $isVerified): static
     {
         $this->isVerified = $isVerified;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): static
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?string
+    {
+        return $this->phone_number;
+    }
+
+    public function setPhoneNumber(?string $phone_number): static
+    {
+        $this->phone_number = $phone_number;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->first_name;
+    }
+
+    public function setFirstName(string $first_name): static
+    {
+        $this->first_name = $first_name;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCodePostal(): ?string
+    {
+        return $this->code_postal;
+    }
+
+    public function setCodePostal(string $code_postal): static
+    {
+        $this->code_postal = $code_postal;
 
         return $this;
     }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,35 +5,98 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-class RegistrationFormType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
+class RegistrationFormType extends AbstractType {
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
+
         $builder
-            ->add('email')
+            ->add('first_name', TextType::class, [
+                'label' => "Prénom",
+                'attr' => ['placeholder' => 'Votre prénom'],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'Votre prénom doit être renseigné']),
+                    new Length(['min' => 2, 'minMessage' => 'Votre prénom doit faire au minimum 2 caractères'])
+                ],
+            ])
+
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+                'attr' => ['placeholder' => 'Votre nom'],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'Votre nom doit être renseigné']),
+                    new Length(['min' => 2, 'minMessage' => 'Votre nom doit faire au minimum 2 caractères' ])],
+            ])
+
+            ->add('phone_number', TelType::class, [
+                'label' => "Numéro de téléphone",
+                'attr' => ['placeholder' => 'Votre numéro de téléphone'],
+                'required' => false,
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez ajouter un numéro de téléphone']),
+                    new Length(['min' => 10, 'minMessage' => 'Votre numéro de téléphone doit être composé de 10 caractères',
+                                'max' => 10, 'maxMessage' => 'Votre numéro de téléphone doit être composé de 10 caractères'])
+                ],
+            ])
+
+            ->add('email', EmailType::class, [
+                'label' => "Email",
+                'attr' => ['placeholder' => 'Votre email'],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'Votre email doit être renseigné']),
+                    new Email(['message' => 'Veuillez renseigner un email valide!']),
+                ],
+            ])
+
+            ->add('address', TextType::class, [
+                'label' => 'Adresse',
+                'attr' => ['placeholder' => 'Votre adresse'],
+                'required' => true,
+                'constraints' => [new NotBlank(['message' => 'Votre adresse doit être renseignée']),],
+            ])
+
+            ->add('code_postal', TextType::class, [
+                'label' => 'Code postal',
+                'attr' => ['placeholder' => 'Votre code postal'],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'Votre code postal doit être renseigné']),
+                    new Length(['min' => 5, 'minMessage' => 'Votre code postal doit faire 5 caractères',
+                                'max' => 5, 'maxMessage' => 'Votre code postal doit faire 5 caractères'])],                                 
+            ])
+
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Veuillez accepter les conditions.',
                     ]),
                 ],
             ])
+
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'label' => 'Mot de passe',
+                'required' => true,
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password', 'placeholder' => 'Votre mot de passe'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez entrer un mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
@@ -42,6 +105,20 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
+            ])
+
+            ->add('newPlainPassword', RepeatedType::class, [
+                'mapped' => false,
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'constraints' => [
+                        new NotBlank(['message' => 'Entrez un Mot de Passe']),
+                        new Length(['min' => 6, 'minMessage' => 'Votre Mot de Passe doit faire au moins {{ limit }} caractères']),
+                    ],
+                    'label' => 'Nouveau Mot de Passe',
+                ],
+                'second_options' => ['label' => 'Répétez le nouveau mot de passe'],
+                'invalid_message' => 'Les mots de passe ne correspondent pas.',
             ])
         ;
     }
